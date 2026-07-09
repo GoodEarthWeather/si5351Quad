7671ccecd9e35d6c7130df287f2afc765b9194bd
GoodEarthWeather <fishineer@gmail.com>
2026-07-09 12:35:24 -0700

    restarted with new project

---
+++ Debug/main.d
@@ -0,0 +1,138 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr4133.h
+main.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/intrinsics.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/driverlib.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+main.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/stdint.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/_ti_config.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/linkage.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/_stdint40.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/stdint.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/cdefs.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/_types.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/machine/_types.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/machine/_stdint.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/_stdint.h
+main.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/stdbool.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/rom_driverlib.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/lcd_e.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/ecomp.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/cs.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/sac.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/timer_b.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/framctl.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/tia.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/tlv.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/timer_a.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/pmm.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/sysctl.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/sfr.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/mpy32.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/wdt_a.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/icc.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/gpio.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/adc.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/crc.h
+main.obj: /home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/rtc.h
+
+../main.c:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr4133.h:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/intrinsics.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/intrinsics_legacy_undefs.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/driverlib.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2xx_4xxgeneric.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/_ti_config.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/linkage.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/_stdint40.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/cdefs.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/_types.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/machine/_types.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/machine/_stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/sys/_stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.2.LTS/include/stdbool.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/rom_driverlib.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/lcd_e.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/ecomp.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/cs.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/sac.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/timer_b.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/framctl.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/tia.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/tlv.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/timer_a.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/pmm.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/sysctl.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/sfr.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/mpy32.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/wdt_a.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/icc.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/gpio.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/adc.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/crc.h:
+
+/home/david/workspace_v12/emptyProject/driverlib/MSP430FR2xx_4xx/rtc.h:
+
